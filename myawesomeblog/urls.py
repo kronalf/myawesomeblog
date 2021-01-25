692a5c26173379bb1ca784239474db2cf8b340bd
Ковалев Дмитрий Георгиевич <kovalev_dg@minudo-home.ru>
2021-01-25 17:28:19 +0300

    Add home page

--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+import events.views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', events.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
