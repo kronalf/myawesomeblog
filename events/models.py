e0a9aff9e08784d7f34c97b6b514f63594ae14fd
Ковалев Дмитрий Георгиевич <kovalev_dg@minudo-home.ru>
2021-01-30 14:02:09 +0300

    Add page blog

--- events/models.py
+++ events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Event(models.Model):
-	event_image = models.ImageField(upload_to = '')
+	event_image = models.ImageField(upload_to = 'event_image/')
 	event_text = models.CharField(max_length = 300)
